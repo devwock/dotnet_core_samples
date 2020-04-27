e618e63799436ee7f929c44189c5163e18174761
devwock <devwock@gmail.com>
2020-04-27 20:24:03 +0900

    Initial Commit

---
+++ Shutdown/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shutdown
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Shutdown.Run(Shutdown.Mode.ForcedShutdown);
+        }
+    }
+}
